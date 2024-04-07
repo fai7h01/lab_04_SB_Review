88e03e708225712c4085550aa03987cd5e3cdac6
fai7h01 <l.varsimashvil10@gmail.com>
2024-04-07 20:44:39 +0400

    config

--- src/main/java/com/cydeo/config/RecipeCreatorConfig.java
+++ src/main/java/com/cydeo/config/RecipeCreatorConfig.java
@@ -1,18 +1,20 @@
 package com.cydeo.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
+@Setter
 @Configuration
-//@ConfigurationProperties("author")
+@ConfigurationProperties("author")
 public class RecipeCreatorConfig {
 
-    @Value("${author.firstName}")
+   // @Value("${author.firstName}")
     String firstName;
-    @Value("${author.lastName}")
+   // @Value("${author.lastName}")
     String lastName;
 
 }
